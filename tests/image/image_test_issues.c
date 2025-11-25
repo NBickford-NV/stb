bfe4b0f777aeb6c4ef4f60eba7e3d4e1277662cc
Nia Bickford <nbickford@nvidia.com>
2025-11-25 00:18:00 -0800

    Add repro case for https://github.com/nothings/stb/issues/1860

--- tests/image/image_test_issues.c
+++ tests/image/image_test_issues.c
@@ -1,10 +1,23 @@
 #include "1454_gif.h"
 #include "1838_gif.h"
+#include "1860_png.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../../stb_image.h"
+#include "stdio.h"
+
+#define CHECK(x)                                                               \
+  {                                                                            \
+    int returncode = (x);                                                      \
+    if (returncode != EXIT_SUCCESS) {                                          \
+      printf("%s failed with code %d!", #x, returncode);                       \
+      return returncode;                                                       \
+    }                                                                          \
+  }
 
 int main() {
-	test_1454_gif();
-	test_1838_gif();
+  CHECK(test_1454_gif());
+  CHECK(test_1838_gif());
+  CHECK(test_1860_png());
+  return EXIT_SUCCESS;
 }
